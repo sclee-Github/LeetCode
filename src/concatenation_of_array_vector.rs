56283818c7baf4fae1c272db0ff738c590727ee8
Scott <shincheol90@gmail.com>
2022-09-14 15:58:12 +0900

    (Vector) Concatenation of Array

---
+++ src/concatenation_of_array_vector.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+        let nums_len = nums.len();
+
+        for i in 0..nums_len {
+            nums.push(nums[i]);
+            // nums.extend([nums[i]]);
+        }
+
+        nums
+    }
+}
