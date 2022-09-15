4b42984f91c7bf8613bb969d9a88748fd46b3160
Scott <shincheol90@gmail.com>
2022-09-15 14:14:08 +0900

    Add unit testing
    Closes : #3

--- src/concatenation_of_array_vector.rs
+++ src/concatenation_of_array_vector.rs
@@ -1,3 +1,27 @@
+pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+    let nums_len = nums.len();
+
+    for i in 0..nums_len {
+        nums.push(nums[i]);
+        // num.extend(nums[i]);
+    }
+
+    nums
+}
+
+#[cfg(test)]
+mod concatenation_of_array {
+    use super::*;
+
+    #[test]
+    fn concatenation() {
+        let vec = vec![1, 2, 3, 1, 2, 3];
+
+        assert_eq!(get_concatenation([1, 2, 3].to_vec()), vec);
+    }
+}
+
+/* First Solution
 impl Solution {
     pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
         let nums_len = nums.len();
@@ -9,3 +33,4 @@ impl Solution {
         nums
     }
 }
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod concatenation_of_array_vector;
 fn main() {
     println!("Hello, world!");
 }
