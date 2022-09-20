6124a6457acf2853cc2f4b1cd83b13d85b069bdb
Scott <shincheol90@gmail.com>
2022-09-20 10:51:20 +0900

    (Vector) Shuffle the array

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod build_array_from_permutation;
 mod concatenation_of_array_vector;
 mod richest_customer_wealth;
 mod running_sum_array;
+mod shuffle_array;
 fn main() {
     println!("Hello, world!");
 }
