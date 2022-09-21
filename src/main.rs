5e3f7d9f17b4b8544d33aafa2adf5ed61f6de97c
Scott <shincheol90@gmail.com>
2022-09-21 12:45:32 +0900

    (Vector) Number of good pairs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod build_array_from_permutation;
 mod concatenation_of_array_vector;
 mod minimum_sum_of_four_digit;
+mod number_of_good_pairs;
 mod richest_customer_wealth;
 mod running_sum_array;
 mod shuffle_array;
